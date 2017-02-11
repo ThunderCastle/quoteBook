51e22cffb0786a7abf5560050c9eb58673c810ec
Thad Paget <fubekafilms@gmail.com>
2017-02-11 11:51:33 -0700

    quote application

---
+++ js/mainCtrl.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    angular.module('quoteBook').controller('mainCtrl', function ($scope, dataService) {
+
+        $scope.quotes = dataService.getQuotes();
+
+        $scope.deleteMe = function (textToDelete) {
+            dataService.removeData(textToDelete);
+        }
+
+        $scope.addQuote = function () {
+            var newQuote = {
+                text: $scope.newQuoteText,
+                author: $scope.newQuoteAuthor
+            }
+            if (dataService.addData(newQuote)) {
+                $scope.newQuoteText = '';
+                $scope.newQuoteAuthor = '';
+            }
+        }
+    });
+
+})();
